c924acedaf34b6838a80abf5d4e8819e407d11d9
Tide-mx <justin.antonio.mtz@gmail.com>
2025-10-07 12:10:47 -0600

    Create contenido.js

---
+++ contenido.js
@@ -0,0 +1,21 @@
+const materiasPorModalidad = {
+  "Primaria": ["Matemáticas","Ciencias","Geografía","Español","Historia","Inglés","Arte","Educación Física"],
+  "Secundaria": ["Álgebra","Física","Química","Historia Universal","Biología","Arte 2","Inglés 2"],
+  "Preparatoria": ["Cálculo","Física Avanzada","Literatura","Química","Historia Moderna","Filosofía","Idiomas"],
+  "Universidad": ["Programación","Economía","Ingeniería","Cálculo Integral","Estadística","Diseño","Psicología"],
+  "Postgrado": ["Gestión de Proyectos","Investigación Avanzada","Filosofía Aplicada","Educación Superior"]
+};
+
+const contenidos = {
+  "Primaria": {
+    "Matemáticas": {
+      "temas":[
+        {titulo:"Sumas Básicas", info:"Aprende a sumar números hasta 100..."},
+        {titulo:"Restas Básicas", info:"Aprende a restar números hasta 100..."},
+        {titulo:"Multiplicación", info:"Conceptos de multiplicación y tablas..."}
+      ]
+    },
+    "Ciencias": { "temas":[ {titulo:"El cuerpo humano", info:"Órganos principales y funciones"}, {titulo:"Animales", info:"Clasificación y características"} ] }
+  },
+  "Secundaria": { "Álgebra": { "temas":[ {titulo:"Ecuaciones de primer grado", info:"Resolver ecuaciones simples"}, {titulo:"Polinomios", info:"Suma, resta y multiplicación"} ] } }
+};
